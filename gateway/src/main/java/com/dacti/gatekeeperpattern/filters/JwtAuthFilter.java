47a1372858076a568eadd40b6ff594e482c12cb2
dacmarcell <dacmarcell@gmail.com>
2026-05-05 03:46:41 -0300

    feat: implement Spring Cloud Gateway with JWT authentication filter

---
+++ gateway/src/main/java/com/dacti/gatekeeperpattern/filters/JwtAuthFilter.java
@@ -0,0 +1,53 @@
+package com.dacti.gatekeeperpattern.filters;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import com.dacti.gatekeeperpattern.utils.JwtUtil;
+
+import reactor.core.publisher.Mono;
+
+@Component
+public class JwtAuthFilter implements GlobalFilter, Ordered {
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain){
+        ServerHttpRequest request = exchange.getRequest();
+        String path = request.getPath().toString();
+
+        if(!path.startsWith("/api")){
+            return chain.filter(exchange);
+        }
+
+        String authHeader = request.getHeaders().getFirst("Authorization");
+        if(authHeader == null || !authHeader.startsWith("Bearer")){
+            return unauthorized(exchange);
+        }
+
+        String token = authHeader.substring(7);
+        if(!jwtUtil.isValid(token)){
+            return unauthorized(exchange);
+        }
+
+        return chain.filter(exchange);
+    }
+
+    private Mono<Void> unauthorized(ServerWebExchange exchange) {
+        exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+        return exchange.getResponse().setComplete();
+    }
+
+    @Override
+    public int getOrder() {
+        return -1;
+    }
+
+}
---
+++ gateway/src/main/java/com/dacti/gatekeeperpattern/utils/JwtUtil.java
@@ -0,0 +1,27 @@
+package com.dacti.gatekeeperpattern.utils;
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtUtil {
+    @Value("${jwt.secret}")
+    private String secret;
+
+    public boolean isValid(String token){
+        try{
+            Jwts.parserBuilder()
+                .setSigningKey(Keys.hmacShaKeyFor(secret.getBytes()))
+                .build()
+                .parseClaimsJws(token);
+
+            return true;
+        }catch(JwtException e){
+            return false;
+        }
+    }
+}
